ea6ae5a479aeb2831eb85c4325df22ea858cc767
memory1 <mshen_85@hotmail.com>
2019-03-28 16:14:31 +0800

    Add project files.

---
+++ HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessage());
+        }
+    [TestMethod]
+    public void TestMethod2()
+    {
+      Assert.AreEqual("Hello", Program.CreateMessage());
+    }
+
+    }
+}
